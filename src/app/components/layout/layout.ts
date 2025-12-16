cff9fea59cb1f14ce39aded4beb37b9bc875e8a9
Allwyn46 <allwyn153@gmail.com>
2025-12-16 18:42:20 +0530

    Completed hero section left side for desktop view

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { Hero } from '../hero/hero';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
