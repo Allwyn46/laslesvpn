189eadf158f53f9fc09b18a46cf45bf3e87aab94
Allwyn46 <allwyn153@gmail.com>
2026-01-14 19:07:45 +0530

    added plan component

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Hero } from '../hero/hero';
 import { About } from '../about/about';
+import { Plan } from '../plan/plan';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar, Hero, About],
+  imports: [Navbar, Hero, About, Plan],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
